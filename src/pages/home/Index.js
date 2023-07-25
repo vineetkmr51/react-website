ba616d9a5b8c7f7552872d63beba9e915fa362d0
Viiniit Aroraa <vineetkmr51@gmail.com>
2023-07-25 16:29:47 +0530

    value section update

--- src/pages/home/Index.js
+++ src/pages/home/Index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './components/Banner'
 import WhoWeAre from './components/WhoWeAre';
+import OurValue from './components/OurValue';
 
 function Home() {
     return (
       <div>
         <Banner/>
         <WhoWeAre/>
+        <OurValue/>
       </div>
     )
   }
---
+++ src/pages/home/components/OurValue.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Value1 from '../../../assets/images/values-1.png';
+import Value2 from '../../../assets/images/values-2.png';
+import Value3 from '../../../assets/images/values-3.png';
+
+function OurValue() {
+  return (
+    <section id="values" className="values">
+
+      <div className="container" data-aos="fade-up">
+
+        <div className="section-header">
+          <h2>Our Values</h2>
+          <p>Odit est perspiciatis laborum et dicta</p>
+        </div>
+
+        <div className="row">
+
+          <div className="col-lg-4" data-aos="fade-up" data-aos-delay="200">
+            <div className="box">
+              <img src={Value1} className="img-fluid" alt=""/>
+              <h3>Ad cupiditate sed est odio</h3>
+              <p>Eum ad dolor et. Autem aut fugiat debitis voluptatem consequuntur sit. Et veritatis id.</p>
+            </div>
+          </div>
+
+          <div className="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="400">
+            <div className="box">
+              <img src={Value2} className="img-fluid" alt=""/>
+              <h3>Voluptatem voluptatum alias</h3>
+              <p>Repudiandae amet nihil natus in distinctio suscipit id. Doloremque ducimus ea sit non.</p>
+            </div>
+          </div>
+
+          <div className="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="600">
+            <div className="box">
+              <img src={Value3} className="img-fluid" alt=""/>
+              <h3>Fugit cupiditate alias nobis.</h3>
+              <p>Quam rem vitae est autem molestias explicabo debitis sint. Vero aliquid quidem commodi.</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
+  );
+}
+
+export default OurValue;
